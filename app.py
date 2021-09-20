e79c31baffa15bf223d9cd2826d6972560151f9d
Anshuman Srivastava <sanu31210@gmail.com>
2021-09-20 20:07:08 +0530

    Some changes in Featrure2

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def math_operation_via_postman():
 
 
 if __name__ == '__main__':
-    host=''127.0.0.1
+    host='33.45.65.7'
     port =8080
     app.run(host="127.0.0.1", port=8080,debug=True) 
 
